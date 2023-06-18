886f2c59e90b9889bfa7ac3a5992f751fea65f60
soumitradev <soumitra.shewale.dev@gmail.com>
2023-06-18 15:12:13 +0530

    feat: Add env vars and env validation

---
+++ config/schema.ts
@@ -0,0 +1,16 @@
+// @ts-check
+import { z } from "zod";
+
+/**
+ * Specify your server-side environment variables schema here.
+ * This way you can ensure the app isn't built with invalid env vars.
+ */
+export const serverSchema = z.object({
+  NODE_ENV: z.enum(["development", "test", "production"]),
+  POSTGRES_USER: z.string().min(1),
+  POSTGRES_PASSWORD: z.string().min(1),
+  POSTGRES_DB: z.string().min(1),
+  POSTGRES_HOST: z.string().min(1),
+  PROD_URL: z.string().url().min(1),
+  PORT: z.coerce.number().default(3000),
+});
