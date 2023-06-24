3273f83da65d9e2b0d79ef85b384f6bf4ac5f14a
majimearun <arunachala0619@gmail.com>
2023-06-24 21:03:19 +0530

    feat: add path to delete endpoint in timetableRouter

--- src/routers/timetableRouter.ts
+++ src/routers/timetableRouter.ts
@@ -3,9 +3,14 @@ import {
   createTimeTableValidator,
   createTimetable,
 } from "../controllers/timetable/create";
+import {
+  deleteTimeTableValidator,
+  deleteTimetable,
+} from "../controllers/timetable/delete";
 
 const timetableRouter = express.Router();
 
 timetableRouter.post("/create", createTimeTableValidator, createTimetable);
+timetableRouter.post("/delete/:id", deleteTimeTableValidator, deleteTimetable);
 
 export default timetableRouter;
