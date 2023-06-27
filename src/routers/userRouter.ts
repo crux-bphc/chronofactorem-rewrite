9c0ae9e0babeb5528282873bb8b75f94425de753
Anurav-Garg <anurav.grg@gmail.com>
2023-06-27 19:06:35 +0530

    feat: add edit user endpoint, and update router

--- src/routers/userRouter.ts
+++ src/routers/userRouter.ts
@@ -1,10 +1,12 @@
 import * as express from "express";
 import { getUserByName } from "../controllers/user/get";
 import { getAllUsers } from "../controllers/user/getAll";
+import { editUser, editUserValidator } from "../controllers/user/editUser";
 
 const userRouter = express.Router();
 
 userRouter.get("/:name", getUserByName);
 userRouter.get("/", getAllUsers);
+userRouter.post("/edit", editUserValidator, editUser);
 
 export default userRouter;
