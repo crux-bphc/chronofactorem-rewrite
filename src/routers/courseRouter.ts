232a4fd61a994912e5c666d2f5bf0e5f6f194084
divok11231 <lkovid@gmail.com>
2023-06-27 13:43:09 +0530

    feat: implement getAll courses route in router file

--- src/routers/courseRouter.ts
+++ src/routers/courseRouter.ts
@@ -5,6 +5,6 @@ import { getCourseByCode } from "../controllers/course/get";
 const courseRouter = express.Router();
 
 courseRouter.get("/:code", getCourseByCode);
-
+courseRouter.get("/course")
 
 export default courseRouter;
