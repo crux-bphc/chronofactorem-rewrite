cc26ff7ff389458c6435cb0c7ce9e0ca955330c4
soumitradev <soumitra.shewale.dev@gmail.com>
2023-06-28 11:37:11 +0530

    build: update .env
    PGUSER is a new required env var, and POSTGRES_SOCKET is no longer needed

--- src/config/schema.ts
+++ src/config/schema.ts
@@ -7,10 +7,10 @@ import { z } from "zod";
  */
 export const serverSchema = z.object({
   NODE_ENV: z.enum(["development", "test", "production"]),
+  PGUSER: z.string().min(1),
   POSTGRES_USER: z.string().min(1),
   POSTGRES_PASSWORD: z.string().min(1),
   POSTGRES_DB: z.string().min(1),
-  POSTGRES_SOCKET: z.string().min(1),
   PROD_URL: z.string().url().min(1),
   PORT: z.coerce.number().default(3000),
 });
