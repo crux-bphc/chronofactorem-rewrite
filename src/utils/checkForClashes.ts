020188204a3a03fd03b49f5119de850ea566808b
majimearun <arunachala0619@gmail.com>
2023-06-29 09:39:20 +0530

    feat: add function that checks for class hour clashes

---
+++ src/utils/checkForClashes.ts
@@ -0,0 +1,19 @@
+import { Timetable } from "../entity/Timetable";
+import { Section } from "../entity/Section";
+
+export const checkForClassHoursClash = (
+  currentTimetable: Timetable,
+  newSection: Section
+) => {
+  const times = currentTimetable.timings;
+  const newRoomTimes = newSection.roomTime;
+
+  const newTimes = newRoomTimes.map((roomTime) => {
+    const [_, day, hour] = roomTime.split(":");
+    return day + hour;
+  });
+
+  const clashes = newTimes.filter((newTime) => {
+    times.includes(newTime);
+  });
+};
