95aeab881853b0cbfa11de699fc773a70c938daf
JustAGhost23 <f20210237@hyderabad.bits-pilani.ac.in>
2023-06-29 13:53:48 +0530

    feat: add path to edit endpoint in timetableRouter

--- src/routers/timetableRouter.ts
+++ src/routers/timetableRouter.ts
@@ -7,10 +7,15 @@ import {
   deleteTimeTableValidator,
   deleteTimetable,
 } from "../controllers/timetable/deleteTimetable";
+import {
+  editTimeTableValidator,
+  editTimetable,
+} from "../controllers/timetable/editTimetable";
 
 const timetableRouter = express.Router();
 
 timetableRouter.post("/create", createTimeTableValidator, createTimetable);
 timetableRouter.post("/delete/:id", deleteTimeTableValidator, deleteTimetable);
+timetableRouter.post("/:id/edit", editTimeTableValidator, editTimetable);
 
 export default timetableRouter;
