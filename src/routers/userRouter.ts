b496b32f03d226370993e8b9e8ec1dc6b630e269
soumitradev <soumitra.shewale.dev@gmail.com>
2023-06-29 17:26:47 +0530

    feat: add getUserDetails endpoint to userRouter

--- src/routers/userRouter.ts
+++ src/routers/userRouter.ts
@@ -1,9 +1,14 @@
 import * as express from "express";
 import { getAllUsers } from "../controllers/user/getAll";
 import { editUser, editUserValidator } from "../controllers/user/editUser";
+import {
+  getUserDetails,
+  getUserDetailsValidator,
+} from "../controllers/user/getUserDetails";
 
 const userRouter = express.Router();
 
+userRouter.get("/:id", getUserDetailsValidator, getUserDetails);
 userRouter.get("/", getAllUsers);
 userRouter.post("/edit", editUserValidator, editUser);
 
