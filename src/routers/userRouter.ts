131b19932e28b25e5c7512caa5ad06a6638baf07
soumitradev <soumitra.shewale.dev@gmail.com>
2023-06-29 16:51:08 +0530

    revert: remove getUserByName from userRouter

--- src/routers/userRouter.ts
+++ src/routers/userRouter.ts
@@ -1,11 +1,9 @@
 import * as express from "express";
-import { getUserByName } from "../controllers/user/get";
 import { getAllUsers } from "../controllers/user/getAll";
 import { editUser, editUserValidator } from "../controllers/user/editUser";
 
 const userRouter = express.Router();
 
-userRouter.get("/:name", getUserByName);
 userRouter.get("/", getAllUsers);
 userRouter.post("/edit", editUserValidator, editUser);
 
