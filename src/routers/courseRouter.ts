32ff4a72548f9e7686ffd951fb6c3de3ad4275f1
divok11231 <lkovid@gmail.com>
2023-06-29 19:39:28 +0530

    fix: add validator

--- src/routers/courseRouter.ts
+++ src/routers/courseRouter.ts
@@ -1,10 +1,11 @@
 import * as express from "express";
 import { getCourseById } from "../controllers/course/getCourseById";
-import { getAllCourses} from "../controllers/course/getAllCourses"
+import { getAllCourses } from "../controllers/course/getAllCourses";
+import { getCourseByIdValidator } from "../controllers/course/getCourseById";
 
 const courseRouter = express.Router();
 
 courseRouter.get("/:id", getCourseById);
-courseRouter.get("/", getAllCourses)
+courseRouter.get("/", getAllCourses, getCourseByIdValidator);
 
 export default courseRouter;
