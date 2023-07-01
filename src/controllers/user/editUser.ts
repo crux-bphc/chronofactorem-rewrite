2ec46e0f56c358f51497b94d3f2779a0266b425d
soumitradev <soumitra.shewale.dev@gmail.com>
2023-07-01 17:10:56 +0530

    refactor: remove unnecessary try catch

--- src/controllers/user/editUser.ts
+++ src/controllers/user/editUser.ts
@@ -37,48 +37,44 @@ const dataSchema = z.object({
 export const editUserValidator = validate(dataSchema);
 
 export const editUser = async (req: Request, res: Response) => {
-  try {
-    const degrees: DegreeList = req.body.degrees;
+  const degrees: DegreeList = req.body.degrees;
 
-    if (degrees.length === 2 && !isAValidDegreeCombination(degrees)) {
-      return res.status(400).json({
-        message: "Dualites may only have one BE degree and one MSc degee",
-      });
-    }
-
-    try {
-      await userRepository.manager.transaction(
-        async (transactionalEntityManager) => {
-          // Update degrees for user
-          const authorId = await transactionalEntityManager
-            .createQueryBuilder()
-            .update("user")
-            .set({ degrees })
-            .where("email = :email", { email: req.body.email })
-            .returning("*")
-            .execute()
-            .then((response) => {
-              return response.raw[0].id;
-            });
-
-          // Update degrees for user's timetables
-          await transactionalEntityManager
-            .createQueryBuilder()
-            .update("timetable")
-            .set({ degrees })
-            .where("authorId = :author", { author: authorId })
-            .execute();
-        }
-      );
-    } catch (err: any) {
-      // will replace the console.log with a logger when we have one
-      console.log("Error editing degrees: ", err.message);
-
-      return res.status(500).json({ message: "Internal Server Error" });
-    }
-
-    return res.json({ message: "User details updated successfully" });
-  } catch (err: any) {
-    throw err;
+  if (degrees.length === 2 && !isAValidDegreeCombination(degrees)) {
+    return res.status(400).json({
+      message: "Dualites may only have one BE degree and one MSc degee",
+    });
   }
+
+  try {
+    await userRepository.manager.transaction(
+      async (transactionalEntityManager) => {
+        // Update degrees for user
+        const authorId = await transactionalEntityManager
+          .createQueryBuilder()
+          .update("user")
+          .set({ degrees })
+          .where("email = :email", { email: req.body.email })
+          .returning("*")
+          .execute()
+          .then((response) => {
+            return response.raw[0].id;
+          });
+
+        // Update degrees for user's timetables
+        await transactionalEntityManager
+          .createQueryBuilder()
+          .update("timetable")
+          .set({ degrees })
+          .where("authorId = :author", { author: authorId })
+          .execute();
+      }
+    );
+  } catch (err: any) {
+    // will replace the console.log with a logger when we have one
+    console.log("Error editing degrees: ", err.message);
+
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+
+  return res.json({ message: "User details updated successfully" });
 };
