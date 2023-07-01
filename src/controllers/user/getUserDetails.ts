bea8d90009ef89bb7fb8a5b8dd565cc6b9fa3e20
soumitradev <soumitra.shewale.dev@gmail.com>
2023-07-01 17:11:35 +0530

    refactor: remove unnecessary try catch

--- src/controllers/user/getUserDetails.ts
+++ src/controllers/user/getUserDetails.ts
@@ -38,51 +38,47 @@ const dataSchema = z.object({
 export const getUserDetailsValidator = validate(dataSchema);
 
 export const getUserDetails = async (req: Request, res: Response) => {
+  const id = req.params.id;
+  const email = req.query.authEmail;
+  let user: User | null = null;
+
   try {
-    const id = req.params.id;
-    const email = req.query.authEmail;
-    let user: User | null = null;
-
-    try {
-      user = await userRepository
-        .createQueryBuilder("user")
-        .leftJoin(
-          "user.timetables",
-          "timetable",
-          "(user.email <> :email and timetable.private = :private and timetable.draft = :draft) or (user.email = :email)",
-          {
-            email: email,
-            draft: false,
-            private: false,
-          }
-        )
-        .select([
-          "user",
-          "timetable.id",
-          "timetable.name",
-          "timetable.degrees",
-          "timetable.private",
-          "timetable.draft",
-          "timetable.archived",
-          "timetable.year",
-          "timetable.acad_year",
-          "timetable.semester",
-          "timetable.created_at",
-          "timetable.last_updated",
-        ])
-        .where("user.id = :id", { id })
-        .getOne();
-    } catch (err: any) {
-      // will replace the console.log with a logger when we have one
-      console.log("Error while querying for user: ", err.message);
-      res.status(500).json({ message: "Internal Server Error" });
-    }
-
-    if (!user) {
-      return res.status(404).json({ message: "User not found" });
-    }
-    return res.json(user);
+    user = await userRepository
+      .createQueryBuilder("user")
+      .leftJoin(
+        "user.timetables",
+        "timetable",
+        "(user.email <> :email and timetable.private = :private and timetable.draft = :draft) or (user.email = :email)",
+        {
+          email: email,
+          draft: false,
+          private: false,
+        }
+      )
+      .select([
+        "user",
+        "timetable.id",
+        "timetable.name",
+        "timetable.degrees",
+        "timetable.private",
+        "timetable.draft",
+        "timetable.archived",
+        "timetable.year",
+        "timetable.acad_year",
+        "timetable.semester",
+        "timetable.created_at",
+        "timetable.last_updated",
+      ])
+      .where("user.id = :id", { id })
+      .getOne();
   } catch (err: any) {
-    throw err;
+    // will replace the console.log with a logger when we have one
+    console.log("Error while querying for user: ", err.message);
+    res.status(500).json({ message: "Internal Server Error" });
   }
+
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
+  return res.json(user);
 };
