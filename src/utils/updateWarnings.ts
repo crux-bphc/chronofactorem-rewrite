bc2eeceb953b518ba04a456ef8b4e20c1217f9e3
majimearun <arunachala0619@gmail.com>
2023-07-01 13:40:17 +0530

    refactor: made all equality checks type safe

--- src/utils/updateWarnings.ts
+++ src/utils/updateWarnings.ts
@@ -25,10 +25,10 @@ export const updateSectionWarnings = (
       // Since warning does not exist already, add a new warning
       const warningSectionTypesList: string[] = requiredSectionTypes.filter(
         (requiredSectionType) => {
-          return requiredSectionType != sectionType;
+          return requiredSectionType !== sectionType;
         }
       );
-      if (warningSectionTypesList.length != 0) {
+      if (warningSectionTypesList.length !== 0) {
         warningMap.set(courseCode, {
           warningSectionTypes: warningSectionTypesList,
         });
@@ -36,12 +36,12 @@ export const updateSectionWarnings = (
     } else {
       // Deleting courseType from warnings after adding course
       for (const currentSectionWarning of currentWarning.warningSectionTypes) {
-        if (sectionType == currentSectionWarning) {
+        if (sectionType === currentSectionWarning) {
           const index = currentWarning.warningSectionTypes.indexOf(
             currentSectionWarning
           );
           currentWarning.warningSectionTypes.splice(index!, 1);
-          if (currentWarning.warningSectionTypes.length == 0) {
+          if (currentWarning.warningSectionTypes.length === 0) {
             warningMap.delete(courseCode);
           }
         }
@@ -66,7 +66,7 @@ export const updateSectionWarnings = (
       // Adding new courseType to warnings after removing course
       if (sectionType in requiredSectionTypes) {
         currentWarningSectionTypes.push(sectionType);
-        if (requiredSectionTypes.length != currentWarningSectionTypes.length) {
+        if (requiredSectionTypes.length !== currentWarningSectionTypes.length) {
           warningMap.set(courseCode, {
             warningSectionTypes: currentWarningSectionTypes,
           });
