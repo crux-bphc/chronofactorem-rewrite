f8daea57b5d1b08c2ca5f44f41d5d8c5acf5d439
soumitradev <soumitra.shewale.dev@gmail.com>
2023-07-14 22:24:57 +0530

    test: add test for getAllCourses

---
+++ src/controllers/course/getAllCourses.test.ts
@@ -0,0 +1,63 @@
+import app from "../../app";
+import { AppDataSource } from "../../db";
+import { Course, courseType } from "../../entity/Course";
+import { courseRepository } from "../../repositories/courseRepository";
+import timetableTestJSON from "../../tests/timetable.test.json";
+import supertest, { Response } from "supertest";
+
+const request = supertest(app);
+
+beforeAll(async () => {
+  await AppDataSource.initialize();
+});
+
+afterAll(async () => {
+  await AppDataSource.destroy();
+});
+
+describe("Test getAllCourses", () => {
+  let allCourses: Map<string, Course> | null = null;
+
+  it("get all courses from db", async () => {
+    allCourses = new Map(
+      (await courseRepository.createQueryBuilder().getMany()).map((course) => [
+        course.id,
+        course,
+      ])
+    );
+  });
+
+  test("Test if database contains non-zero courses", () => {
+    expect(allCourses).toBeTruthy();
+  });
+
+  test("Test if database contains all courses from JSON", () => {
+    expect(allCourses?.size).toBe(
+      Object.keys(timetableTestJSON.courses).length
+    );
+  });
+
+  describe("Test getAllCourses 200", () => {
+    describe("Test response", () => {
+      let response: Response | null = null;
+
+      it("Make API call", async () => {
+        response = await request.get("/course/");
+      });
+
+      test("Test if returns truthy response", () => {
+        expect(response?.body).not.toHaveLength(0);
+      });
+
+      test("Test if returns response status 200", () => {
+        expect(response?.status).toEqual(200);
+      });
+
+      test("Test if all courses are valid", () => {
+        expect(() =>
+          courseType.array().min(0).parse(response?.body)
+        ).not.toThrow();
+      });
+    });
+  });
+});
