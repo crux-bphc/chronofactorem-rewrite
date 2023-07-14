18c66bc40eec4ed534c57ddad1286cf867f54b37
soumitradev <soumitra.shewale.dev@gmail.com>
2023-07-14 22:19:29 +0530

    refactor: allow section type enums to have a name

--- src/types/sectionTypes.ts
+++ src/types/sectionTypes.ts
@@ -1,11 +1,19 @@
 import { z } from "zod";
+import { addNameToString } from "./zodFieldTypes";
 
 // !!! IMPORTANT: THIS IS THE SOURCE OF TRUTH FOR SECTION TYPES
 export const ApprovedSectionTypeList = ["L", "P", "T"] as const;
 
-export const SectionTypeZodEnum = z.enum(ApprovedSectionTypeList);
+export const NamedSectionTypeZodEnum = (name?: string) =>
+  z.enum(ApprovedSectionTypeList, {
+    required_error: addNameToString("section type is required", name),
+    invalid_type_error: addNameToString("section type is not valid", name),
+  });
+export const SectionTypeZodEnum = NamedSectionTypeZodEnum();
 
-export const SectionTypeZodList = SectionTypeZodEnum.array();
+export const NamedSectionTypeZodList = (name?: string) =>
+  NamedSectionTypeZodEnum(name).array();
+export const SectionTypeZodList = NamedSectionTypeZodList();
 
 export type SectionTypeEnum = z.infer<typeof SectionTypeZodEnum>;
 
