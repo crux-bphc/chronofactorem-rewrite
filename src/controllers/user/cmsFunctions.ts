25c87b8040a503fa9d5beca06ca270c1697e0d1f
soumitradev <soumitra.shewale.dev@gmail.com>
2023-08-05 13:31:36 +0530

    feat: add unenroll endpoint

---
+++ src/controllers/user/cmsFunctions.ts
@@ -0,0 +1,41 @@
+import { Request, Response } from "express";
+import { validate } from "../../middleware/zodValidateRequest";
+import { z } from "zod";
+import {
+  namedIntegerType,
+  namedNonEmptyStringType,
+} from "../../types/zodFieldTypes";
+import fetch from "cross-fetch";
+
+const unenrollDataSchema = z.object({
+  body: z.object({
+    enrollID: namedIntegerType("enrollID"),
+    sesskey: namedNonEmptyStringType("sesskey"),
+    cookie: namedNonEmptyStringType("cookie"),
+  }),
+});
+
+export const unenrollValidator = validate(unenrollDataSchema);
+
+export const unenroll = async (req: Request, res: Response) => {
+  const enrollID: number = req.body.enrollID;
+  const sesskey: string = req.body.sesskey;
+  const cookie: string = req.body.cookie;
+
+  try {
+    const unenrolResponse = await fetch(
+      `https://cms.bits-hyderabad.ac.in/enrol/self/unenrolself.php?confirm=1&enrolid=${enrollID}&sesskey=${sesskey}`,
+      {
+        method: "GET",
+        headers: {
+          cookie: `MoodleSession=${cookie};`,
+        },
+      }
+    );
+    return res.status(unenrolResponse.status).end();
+  } catch (err: any) {
+    // will replace the console.log with a logger when we have one
+    console.log("Error unenrolling from course: ", err.message);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+};
