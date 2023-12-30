5f5342b9562b46ea5a015c9dc953f0ebbc26b84c
soumitradev <soumitra.shewale.dev@gmail.com>
2023-12-30 12:54:09 +0530

    fix: update Login page to use correct color variables

--- frontend/src/Login.tsx
+++ frontend/src/Login.tsx
@@ -1,6 +1,8 @@
 import { Route } from "@tanstack/react-router";
 import { AtSign } from "lucide-react";
 import { rootRoute } from "./main";
+import { Button } from "./components/ui/button";
+import { ModeToggle } from "./components/mode-toggle";
 
 const loginRoute = new Route({
   getParentRoute: () => rootRoute,
@@ -11,30 +13,37 @@ const loginRoute = new Route({
 function Login() {
   return (
     <>
-      <div className="flex bg-slate-950 h-screen w-full justify-center items-center">
+      <div className="flex bg-background h-screen w-full justify-center items-center">
+        <div className="fixed top-4 right-4">
+          <ModeToggle />
+        </div>
         <div className="flex flex-col items-center">
-          <h2 className="scroll-m-20 md:text-lg text-md tracking-tight lg:text-xl text-slate-400">
+          <h2 className="scroll-m-20 md:text-lg text-md tracking-tight lg:text-xl text-muted-foreground">
             Welcome to
           </h2>
-          <h1 className="scroll-m-20 md:text-6xl text-4xl font-extrabold tracking-tight lg:text-7xl text-slate-50">
+          <h1 className="scroll-m-20 md:text-6xl text-4xl font-extrabold tracking-tight lg:text-7xl text-foreground">
             ChronoFactorem
           </h1>
-          <a
-            href={"/api/auth/google"}
-            className="w-fit md:text-lg text-sm md:mt-6 mt-2 bg-slate-700 text-slate-50 rounded-lg flex px-4 py-2 items-center font-bold hover:bg-slate-600 transition ease-in-out"
+          <Button
+            asChild
+            className="md:mt-6 mt-2 rounded-lg flex px-4 py-2 items-center"
           >
-            <AtSign className="mr-2 md:h-4 d:w-4 w-3 h-3" />
-            Login with Google
-          </a>
+            <a href="/api/auth/google">
+              <AtSign className="mr-2 md:h-4 md:w-4 w-3 h-3" />
+              <span className="font-bold md:text-lg text-sm">
+                Login with Google
+              </span>
+            </a>
+          </Button>
           <a
             href="/about"
-            className="w-fit md:mt-8 mt-2 text-slate-400 md:text-lg text-sm hover:underline underline-offset-4"
+            className="w-fit md:mt-8 mt-2 text-muted-foreground md:text-lg text-sm hover:underline underline-offset-4"
           >
             About ChronoFactorem
           </a>
         </div>
       </div>
-      <span className="fixed bottom-0 bg-slate-800 w-full text-center py-1 text-xs md:px-0 px-8 tracking-tight md:text-lg text-slate-400">
+      <span className="fixed bottom-0 bg-muted w-full text-center py-1 text-xs md:px-0 px-8 tracking-tight md:text-lg text-muted-foreground">
         Powered by CRUx: The Programming and Computing Club of BITS Hyderabad
       </span>
     </>
