c143abbc8775520c2454f3fc45c41670a59f8a0e
Anurav-Garg <anurav.grg@gmail.com>
2023-12-30 16:01:48 +0530

    feat: add 'archived' query param to getAllCourses

--- backend/src/controllers/course/getAllCourses.ts
+++ backend/src/controllers/course/getAllCourses.ts
@@ -1,11 +1,31 @@
 import { Request, Response } from "express";
+import { z } from "zod";
+import { Course } from "../../entity/Course";
+import { validate } from "../../middleware/zodValidateRequest";
 import { courseRepository } from "../../repositories/courseRepository";
 
+const dataSchema = z.object({
+  query: z.object({
+    archived: z.union([z.literal("true"), z.literal("false")]).optional(),
+  }),
+});
+
+export const getAllCoursesValidator = validate(dataSchema);
+
 export const getAllCourses = async (req: Request, res: Response) => {
+  const archived: boolean = (req.query.archived as string) === "true";
+
   try {
-    const courses = await courseRepository
-      .createQueryBuilder("course")
-      .getMany();
+    let courses: Course[];
+
+    if (archived) {
+      courses = await courseRepository.createQueryBuilder("course").getMany();
+    } else {
+      courses = await courseRepository
+        .createQueryBuilder("course")
+        .where("course.archived = :archived", { archived: archived })
+        .getMany();
+    }
 
     if (courses.length === 0) {
       return res.status(404).json({ message: "Courses do not not exist" });
