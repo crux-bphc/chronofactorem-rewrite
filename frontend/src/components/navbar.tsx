68b8469896351990d89ccba782544a9781c78576
JustAGhost23 <f20210237@hyderabad.bits-pilani.ac.in>
2023-12-31 18:17:06 +0530

    feat: complete navbar with styling

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,10 +1,18 @@
-import { queryOptions, useQuery } from "@tanstack/react-query";
+import {
+  queryOptions,
+  useMutation,
+  useQuery,
+  useQueryClient,
+} from "@tanstack/react-query";
 import { Link } from "@tanstack/react-router";
-import axios  from "axios";
+import axios, { AxiosError } from "axios";
 import { z } from "zod";
 import { userWithTimetablesType } from "../../../lib";
+import { LogOut, Pencil, Info, Plus } from "lucide-react";
+import ViteSvg from "../../public/vite.svg";
 import { router } from "../main";
-import { LogOut } from "lucide-react";
+import { ToastAction } from "@/components/ui/toast";
+import { useToast } from "@/components/ui/use-toast";
 import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
@@ -13,6 +21,7 @@ import {
   DropdownMenuTrigger,
   DropdownMenuLabel,
 } from "@/components/ui/dropdown-menu";
+import { ModeToggle } from "./mode-toggle";
 
 const fetchUserDetails = async (): Promise<
   z.infer<typeof userWithTimetablesType>
@@ -35,6 +44,91 @@ const userQueryOptions = queryOptions({
 
 export function NavBar() {
   const userQueryResult = useQuery(userQueryOptions);
+  const { toast } = useToast();
+  const queryClient = useQueryClient();
+  const createMutation = useMutation({
+    mutationFn: () => {
+      return axios.post<{ message: string; id: number }>(
+        "/api/timetable/create",
+      );
+    },
+    onSuccess: (response) => {
+      queryClient.invalidateQueries({ queryKey: ["user"] });
+      router.navigate({ to: `/edit/${response.data.id}` });
+    },
+    onError: (error) => {
+      if (error instanceof AxiosError && error.response) {
+        if (error.response.status === 401) {
+          router.navigate({ to: "/login" });
+        }
+        if (error.response.status === 400) {
+          toast({
+            title: "Error",
+            description:
+              "message" in error.response.data
+                ? error.response.data.message
+                : "API returned 400",
+            variant: "destructive",
+            action: (
+              <ToastAction altText="Report issue: https://github.com/crux-bphc/chronofactorem-rewrite/issues">
+                <a href="https://github.com/crux-bphc/chronofactorem-rewrite/issues">
+                  Report
+                </a>
+              </ToastAction>
+            ),
+          });
+        } else if (error.response.status === 404) {
+          toast({
+            title: "Error",
+            description:
+              "message" in error.response.data
+                ? error.response.data.message
+                : "API returned 404",
+            variant: "destructive",
+            action: (
+              <ToastAction altText="Report issue: https://github.com/crux-bphc/chronofactorem-rewrite/issues">
+                <a href="https://github.com/crux-bphc/chronofactorem-rewrite/issues">
+                  Report
+                </a>
+              </ToastAction>
+            ),
+          });
+        } else if (error.response.status === 500) {
+          toast({
+            title: "Server Error",
+            description:
+              "message" in error.response.data
+                ? error.response.data.message
+                : "API returned 500",
+            variant: "destructive",
+            action: (
+              <ToastAction altText="Report issue: https://github.com/crux-bphc/chronofactorem-rewrite/issues">
+                <a href="https://github.com/crux-bphc/chronofactorem-rewrite/issues">
+                  Report
+                </a>
+              </ToastAction>
+            ),
+          });
+        } else {
+          toast({
+            title: "Unknown Error",
+            description:
+              "message" in error.response.data
+                ? error.response.data.message
+                : `API returned ${error.response.status}`,
+            variant: "destructive",
+            action: (
+              <ToastAction altText="Report issue: https://github.com/crux-bphc/chronofactorem-rewrite/issues">
+                <a href="https://github.com/crux-bphc/chronofactorem-rewrite/issues">
+                  Report
+                </a>
+              </ToastAction>
+            ),
+          });
+        }
+      }
+    },
+  });
 
   if (userQueryResult.isFetching) {
     return <span>Loading...</span>;
@@ -64,57 +158,69 @@ export function NavBar() {
   }
 
   return (
-    <div className="flex w-full justify-between">
+    <div className="flex w-full justify-between shadow-lg">
       <div className="flex items-center">
         <Link to="/">
-          <h1 className="scroll-m-20 cursor-pointer text-2xl font-extrabold tracking-tight lg:text-3xl m-4 text-slate-50">
-            Chronofactorem<sup>ᵝ</sup>
-          </h1>
-        </Link>
-        <Button className="text-green-200 w-fit text-xl p-4 ml-4 bg-green-900 hover:bg-green-800">
-          Create a timetable
-        </Button>
-        <a
-          href="/about"
-          className="text-slate-300 py-2 px-4 ml-4 text-lg rounded-full hover:bg-slate-800 transition h-fit duration-200 ease-in-out"
-        >
-          About
-        </a>
-        <a
-          href="/cmsExport"
-          className="text-slate-300 py-2 px-4 ml-4 text-lg rounded-full hover:bg-slate-800 transition h-fit duration-200 ease-in-out"
-        >
-          CMS Auto-Enroll
-        </a>
-      </div>
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <div className="rounded-full text-slate-50 bg-slate-500 p-1 px-3 text-xl h-fit mx-8 mt-4">
-            <span>{userQueryResult.data.name[0]}</span>
+          <div className="hidden md:flex">
+            <h1 className="scroll-m-20 cursor-pointer text-2xl font-extrabold tracking-tight lg:text-3xl m-4 text-foreground">
+              ChronoFactorem<sup>™</sup>
+            </h1>
           </div>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent className="w-56 bg-slate-800 text-slate-300 border-slate-700">
-          <DropdownMenuLabel>My Account</DropdownMenuLabel>
-          <DropdownMenuItem
-            className="focus:bg-red-700 focus:text-red-100 cursor-pointer"
-            onClick={() => {
-              fetch(`${import.meta.env.VITE_BACKEND_URL}/auth/logout`, {
-                method: "GET",
-                headers: {
-                  "Access-Control-Allow-Origin": "*",
-                  "Content-Type": "application/json",
-                },
-                mode: "cors",
-                credentials: "include",
-              });
-              router.navigate({ to: "/login" });
-            }}
-          >
-            <LogOut className="mr-2 h-4 w-4" />
-            <span>Log out</span>
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
+          <div className="flex md:hidden">
+            <h1 className="scroll-m-20 cursor-pointer text-2xl font-extrabold tracking-tight lg:text-3xl m-4 text-foreground">
+              Chrono<sup>™</sup>
+            </h1>
+          </div>
+        </Link>
+        <Button
+          className="text-green-200 w-fit text-xl p-4 ml-4 bg-green-900 hover:bg-green-800"
+          onClick={() => createMutation.mutate()}
+        >
+          <div className="hidden md:flex">Create a timetable</div>
+          <div className="flex md:hidden">
+            <Plus className="h-6 w-6" />
+          </div>
+        </Button>
+        <Link
+          to="/about"
+          className="text-primary py-2 px-4 ml-4 text-lg rounded-full hover:bg-muted transition h-fit duration-200 ease-in-out"
+        >
+          <div className="hidden md:flex">About</div>
+          <div className="flex md:hidden">
+            <Info className="h-6 w-6" />
+          </div>
+        </Link>
+      </div>
+      <div className="flex flex-row">
+        <div className="pt-3">
+          <ModeToggle />
+        </div>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <div className="rounded-full text-foreground bg-accent p-1 px-3 text-xl h-fit lg:mx-8 mx-4 mt-4">
+              <span>{userQueryResult.data.name[0]}</span>
+            </div>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="w-56">
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuItem asChild>
+              <Link to="/editProfile">
+                <Pencil className="mr-2 h-4 w-4" />
+                <span>Edit Profile</span>
+              </Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem
+              asChild
+              className="focus:bg-destructive/90 focus:text-destructive-foreground cursor-pointer"
+            >
+              <Link to="/login">
+                <LogOut className="mr-2 h-4 w-4" />
+                <span>Log out</span>
+              </Link>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
     </div>
   );
 }
