0d856a890b55e19ed24f8c42c56d87b145d69af9
JustAGhost23 <f20210237@hyderabad.bits-pilani.ac.in>
2023-12-31 13:50:39 +0530

    feat: display navbar on screen

--- frontend/src/RootComponent.tsx
+++ frontend/src/RootComponent.tsx
@@ -2,11 +2,13 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import { Toaster } from "./components/ui/toaster";
+import { NavBar } from "./components/navbar";
 
 export default function RootComponent() {
   return (
     <>
       <Toaster />
+      <NavBar />
       <Outlet />
       <ReactQueryDevtools buttonPosition="bottom-right" />
       <TanStackRouterDevtools position="bottom-left" />
