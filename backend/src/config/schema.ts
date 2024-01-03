b00772f63bd946e166ecf2c55927d2ae016a1e68
soumitradev <soumitra.shewale.dev@gmail.com>
2024-01-03 14:36:26 +0530

    build: change FRONTEND_PORT to FRONTEND_DEV_PORT to make name clearer

--- backend/src/config/schema.ts
+++ backend/src/config/schema.ts
@@ -20,6 +20,6 @@ export const serverSchema = z.object({
     .string()
     .regex(/^\d+-[a-z0-9]+\.apps\.googleusercontent\.com$/),
   GOOGLE_CLIENT_SECRET: z.string().min(1),
-  FRONTEND_PORT: z.coerce.number().default(5173),
+  FRONTEND_DEV_PORT: z.coerce.number().default(5173),
   NGINX_PORT: z.coerce.number().default(80),
 });
