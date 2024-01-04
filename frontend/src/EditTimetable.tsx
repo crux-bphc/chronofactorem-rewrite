280e1727a743efd72c96c9111170a2d2303af5c4
soumitradev <soumitra.shewale.dev@gmail.com>
2024-01-05 00:02:13 +0530

    fix: fix colors in some places

--- frontend/src/EditTimetable.tsx
+++ frontend/src/EditTimetable.tsx
@@ -782,13 +782,13 @@ function EditTimetable() {
                 <Tooltip delayDuration={100}>
                   <TooltipTrigger
                     asChild
-                    className="hover:bg-slate-700 transition duration-200 ease-in-out"
+                    className="hover:bg-accent hover:text-accent-foreground transition duration-200 ease-in-out"
                   >
                     <div className="p-2 rounded-full">
                       <AlertOctagon className="w-6 h-6 m-1" />
                     </div>
                   </TooltipTrigger>
-                  <TooltipContent className="bg-muted text-foreground border-muted-foreground text-md">
+                  <TooltipContent className="bg-muted text-foreground border-muted-foreground/40 text-md">
                     {missingCDCs.length > 0 && (
                       <div className="flex flex-col">
                         <span>
@@ -803,7 +803,7 @@ function EditTimetable() {
                               onClick={() => {
                                 handleMissingCDCClick(e.id);
                               }}
-                              className="p-2 w-fit h-fit ml-2 mb-1 bg-transparent hover:bg-slate-700 rounded-full"
+                              className="p-2 w-fit h-fit ml-2 mb-1 bg-transparent hover:bg-slate-300 dark:hover:bg-slate-700 text-secondary-foreground rounded-full"
                             >
                               <ArrowUpRightFromCircle className="w-4 h-4" />
                             </Button>
@@ -821,7 +821,7 @@ function EditTimetable() {
                           Please report this issue
                           <a
                             href="https://github.com/crux-bphc/chronofactorem-rewrite/issues"
-                            className="text-blue-300 flex pl-1"
+                            className="text-blue-700 dark:text-blue-400 flex pl-1"
                           >
                             here
                             <ArrowUpRightFromCircle className="w-4 h-4 ml-1" />
@@ -914,7 +914,7 @@ function EditTimetable() {
                 <Tooltip delayDuration={100}>
                   <TooltipTrigger
                     asChild
-                    className="duration-200 mr-4 text-md p-2 h-fit hover:bg-orange-800/40 rounded-lg px-4"
+                    className="duration-200 mr-4 text-md p-2 h-fit dark:hover:bg-orange-800/40 hover:bg-orange-300/40 rounded-lg px-4"
                   >
                     <div className="flex items-center">
                       <span className="text-orange-400 pr-4">
@@ -937,7 +937,7 @@ function EditTimetable() {
                       <AlertTriangle className="w-6 h-6 m-1 text-orange-400" />
                     </div>
                   </TooltipTrigger>
-                  <TooltipContent className="bg-muted text-foreground border-muted-foreground text-md">
+                  <TooltipContent className="bg-muted text-foreground border-muted-foreground/40 text-md">
                     {timetable.warnings.map((warning) => (
                       <div className="pb-2" key={warning}>
                         <span className="font-bold">
@@ -963,7 +963,7 @@ function EditTimetable() {
                                       )[0],
                                     );
                                   }}
-                                  className="p-2 w-fit h-fit ml-2 mb-1 bg-transparent hover:bg-slate-700 rounded-full"
+                                  className="p-2 w-fit h-fit ml-2 mb-1 bg-transparent hover:bg-slate-300 dark:hover:bg-slate-700 text-secondary-foreground rounded-full"
                                 >
                                   <ArrowUpRightFromCircle className="w-4 h-4" />
                                 </Button>
