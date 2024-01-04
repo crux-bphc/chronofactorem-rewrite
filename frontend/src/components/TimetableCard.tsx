b67561851f22700f66b323f58b2e3a81fa987089
pnicto <f20210264@hyderabad.bits-pilani.ac.in>
2024-01-04 15:52:21 +0530

    feat: add tooltips to edit, delete buttons

--- frontend/src/components/TimetableCard.tsx
+++ frontend/src/components/TimetableCard.tsx
@@ -20,6 +20,12 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { ToastAction } from "@/components/ui/toast";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 import * as AlertDialogPrimitive from "@radix-ui/react-alert-dialog";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import axios, { AxiosError } from "axios";
@@ -221,144 +227,167 @@ function TimetableCard({ timetable, showFooter }: Props) {
   });
 
   return (
-    <Card className="min-h-60 flex flex-col min-w-80 shadow-lg">
-      <CardHeader className="pb-2">
-        <CardTitle className="text-xl md:text-2xl flex justify-between items-center">
-          {timetable.name}
-          {timetable.archived && (timetable.private ? <EyeOff /> : <Eye />)}
-        </CardTitle>
-      </CardHeader>
-      <CardContent>
-        <Badge variant="default" className="w-fit">
-          <p className="flex items-center gap-1">
-            <span>{timetable.acadYear}</span>
-            <span>|</span>
-            <span>{timetable.degrees.join("")}</span>
-            <span>|</span>
-            <span className="flex-none">{`${timetable.year}-${timetable.semester}`}</span>
-          </p>
-        </Badge>
-      </CardContent>
-      {showFooter && (
-        <CardFooter className="flex justify-end gap-2 mt-auto">
-          {!timetable.draft && (
-            <Button
-              variant="outline"
-              onClick={() =>
-                editMutation.mutate({
-                  name: timetable.name,
-                  isPrivate: !timetable.private,
-                  isDraft: timetable.draft,
-                })
-              }
-            >
-              Make {timetable.private ? "Public" : "Private"}
-            </Button>
-          )}
+    <TooltipProvider>
+      <Card className="min-h-60 flex flex-col min-w-80 shadow-lg">
+        <CardHeader className="pb-2">
+          <CardTitle className="text-xl md:text-2xl flex justify-between items-center">
+            {timetable.name}
+            {timetable.archived && (timetable.private ? <EyeOff /> : <Eye />)}
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <Badge variant="default" className="w-fit">
+            <p className="flex items-center gap-1">
+              <span>{timetable.acadYear}</span>
+              <span>|</span>
+              <span>{timetable.degrees.join("")}</span>
+              <span>|</span>
+              <span className="flex-none">{`${timetable.year}-${timetable.semester}`}</span>
+            </p>
+          </Badge>
+        </CardContent>
+        {showFooter && (
+          <CardFooter className="flex justify-end gap-2 mt-auto">
+            {!timetable.draft && (
+              <Button
+                variant="outline"
+                onClick={() =>
+                  editMutation.mutate({
+                    name: timetable.name,
+                    isPrivate: !timetable.private,
+                    isDraft: timetable.draft,
+                  })
+                }
+              >
+                Make {timetable.private ? "Public" : "Private"}
+              </Button>
+            )}
 
-          {!timetable.archived ? (
-            <Button
-              variant="ghost"
-              className="rounded-full p-3"
-              onClick={() =>
-                editMutation.mutate({
-                  name: timetable.name,
-                  isPrivate: true,
-                  isDraft: true,
-                })
-              }
-            >
-              <Edit2 />
-            </Button>
-          ) : (
-            <Dialog>
-              <DialogTrigger asChild>
-                <Button variant="ghost" className="rounded-full p-3">
-                  <Edit2 />
-                </Button>
-              </DialogTrigger>
-              <DialogContent className="sm:max-w-[425px]">
-                <DialogHeader>
-                  <DialogTitle>Edit Archived Timetable</DialogTitle>
-                </DialogHeader>
-                <div className="grid gap-4 py-4">
-                  <div className="grid grid-cols-4 items-center gap-4">
-                    <Label htmlFor="name" className="text-right">
-                      Name
-                    </Label>
-                    <Input
-                      id="name"
-                      value={timetableName ?? timetable.name}
-                      onChange={(e) => setTimetableName(e.target.value)}
-                      className="col-span-3"
-                    />
-                  </div>
-                  <div className="mx-auto">
-                    <div className="flex items-center space-x-2">
-                      <Switch
-                        id="visibility"
-                        checked={timetableVisibility ?? timetable.private}
-                        onCheckedChange={setTimetableVisibility}
+            {!timetable.archived ? (
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <Button
+                    variant="ghost"
+                    className="rounded-full p-3"
+                    onClick={() =>
+                      editMutation.mutate({
+                        name: timetable.name,
+                        isPrivate: true,
+                        isDraft: true,
+                      })
+                    }
+                  >
+                    <Edit2 />
+                  </Button>
+                </TooltipTrigger>
+                <TooltipContent>
+                  <p>Edit Timetable</p>
+                </TooltipContent>
+              </Tooltip>
+            ) : (
+              <Dialog>
+                <Tooltip>
+                  <DialogTrigger asChild>
+                    <TooltipTrigger asChild>
+                      <Button variant="ghost" className="rounded-full p-3">
+                        <Edit2 />
+                      </Button>
+                    </TooltipTrigger>
+                  </DialogTrigger>
+                  <TooltipContent>
+                    <p>Edit Timetable</p>
+                  </TooltipContent>
+                </Tooltip>
+                <DialogContent className="sm:max-w-[425px]">
+                  <DialogHeader>
+                    <DialogTitle>Edit Archived Timetable</DialogTitle>
+                  </DialogHeader>
+                  <div className="grid gap-4 py-4">
+                    <div className="grid grid-cols-4 items-center gap-4">
+                      <Label htmlFor="name" className="text-right">
+                        Name
+                      </Label>
+                      <Input
+                        id="name"
+                        value={timetableName ?? timetable.name}
+                        onChange={(e) => setTimetableName(e.target.value)}
+                        className="col-span-3"
                       />
-                      <Label htmlFor="visibility">Private</Label>
+                    </div>
+                    <div className="mx-auto">
+                      <div className="flex items-center space-x-2">
+                        <Switch
+                          id="visibility"
+                          checked={timetableVisibility ?? timetable.private}
+                          onCheckedChange={setTimetableVisibility}
+                        />
+                        <Label htmlFor="visibility">Private</Label>
+                      </div>
                     </div>
                   </div>
-                </div>
-                <DialogFooter>
-                  <DialogClose asChild>
-                    <Button
-                      type="submit"
-                      onClick={() => {
-                        editMutation.mutate({
-                          name: timetableName ?? timetable.name,
-                          isPrivate: timetableVisibility ?? timetable.private,
-                          isDraft: false,
-                        });
-                      }}
-                    >
-                      Save changes
-                    </Button>
-                  </DialogClose>
-                </DialogFooter>
-              </DialogContent>
-            </Dialog>
-          )}
+                  <DialogFooter>
+                    <DialogClose asChild>
+                      <Button
+                        type="submit"
+                        onClick={() => {
+                          editMutation.mutate({
+                            name: timetableName ?? timetable.name,
+                            isPrivate: timetableVisibility ?? timetable.private,
+                            isDraft: false,
+                          });
+                        }}
+                      >
+                        Save changes
+                      </Button>
+                    </DialogClose>
+                  </DialogFooter>
+                </DialogContent>
+              </Dialog>
+            )}
 
-          <AlertDialog>
-            <AlertDialogTrigger asChild>
-              <Button
-                variant="ghost"
-                className="rounded-full p-3 hover:bg-destructive/90 hover:text-destructive-foreground"
-              >
-                <Trash />
-              </Button>
-            </AlertDialogTrigger>
-            <AlertDialogContent className="p-8">
-              <AlertDialogHeader>
-                <AlertDialogTitle className="text-2xl">
-                  Are you sure?
-                </AlertDialogTitle>
-                <AlertDialogDescription className="text-destructive text-lg font-bold">
-                  All your progress on this timetable will be lost, and
-                  unrecoverable.
-                </AlertDialogDescription>
-              </AlertDialogHeader>
-              <AlertDialogFooter>
-                <AlertDialogCancel>Cancel</AlertDialogCancel>
-                <AlertDialogPrimitive.Action asChild>
-                  <Button
-                    variant="destructive"
-                    onClick={() => deleteMutation.mutate()}
-                  >
-                    Delete
-                  </Button>
-                </AlertDialogPrimitive.Action>
-              </AlertDialogFooter>
-            </AlertDialogContent>
-          </AlertDialog>
-        </CardFooter>
-      )}
-    </Card>
+            <AlertDialog>
+              <Tooltip>
+                <AlertDialogTrigger asChild>
+                  <TooltipTrigger asChild>
+                    <Button
+                      variant="ghost"
+                      className="rounded-full p-3 hover:bg-destructive/90 hover:text-destructive-foreground"
+                    >
+                      <Trash />
+                    </Button>
+                  </TooltipTrigger>
+                </AlertDialogTrigger>
+                <TooltipContent>
+                  <p>Delete Timetable</p>
+                </TooltipContent>
+              </Tooltip>
+              <AlertDialogContent className="p-8">
+                <AlertDialogHeader>
+                  <AlertDialogTitle className="text-2xl">
+                    Are you sure?
+                  </AlertDialogTitle>
+                  <AlertDialogDescription className="text-destructive text-lg font-bold">
+                    All your progress on this timetable will be lost, and
+                    unrecoverable.
+                  </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                  <AlertDialogCancel>Cancel</AlertDialogCancel>
+                  <AlertDialogPrimitive.Action asChild>
+                    <Button
+                      variant="destructive"
+                      onClick={() => deleteMutation.mutate()}
+                    >
+                      Delete
+                    </Button>
+                  </AlertDialogPrimitive.Action>
+                </AlertDialogFooter>
+              </AlertDialogContent>
+            </AlertDialog>
+          </CardFooter>
+        )}
+      </Card>
+    </TooltipProvider>
   );
 }
 
