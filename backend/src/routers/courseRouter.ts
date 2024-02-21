f6f81f653d2c2a59f399cf7a364625876c49959e
kishan1735 <gswwarriorsnba@gmail.com>
2024-02-21 23:37:15 +0530

    feat: update timetable and sections table when there is change in course timings

--- backend/src/routers/courseRouter.ts
+++ backend/src/routers/courseRouter.ts
@@ -5,10 +5,12 @@ import {
 } from "../controllers/course/getAllCourses.js";
 import { getCourseById } from "../controllers/course/getCourseById.js";
 import { getCourseByIdValidator } from "../controllers/course/getCourseById.js";
+import { updateChangedTimetable } from "../controllers/timetable/updateChangedTimetable.js";
 
 const courseRouter = express.Router();
 
 courseRouter.get("/", getAllCoursesValidator, getAllCourses);
 courseRouter.get("/:id", getCourseByIdValidator, getCourseById);
+courseRouter.post("/update", updateChangedTimetable);
 
 export default courseRouter;
