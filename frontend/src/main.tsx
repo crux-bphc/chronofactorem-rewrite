59393055882f0ca9704152291a8d1fcb3c2d4779
Namit Bhutani <namit.bhut@gmail.com>
2024-06-23 22:21:59 +0530

    refactor: rename Finalize.tsx, Home.tsx route

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -15,9 +15,9 @@ import cmsRoute from "./Cms";
 import cmsExportRoute from "./CmsExport";
 import editTimetableRoute from "./EditTimetable";
 import editUserProfileRoute from "./EditUserProfile";
-import finalizeTimetableRoute from "./Finalize";
+import finalizeTimetableRoute from "./FinalizeTimetable";
 import getDegreesRoute from "./GetDegrees";
-import indexRoute from "./Home";
+import homeRoute from "./Home";
 import loginRoute from "./Login";
 import notFoundRoute from "./NotFound";
 import RootComponent from "./RootComponent";
@@ -43,7 +43,7 @@ const routeTree = rootRoute.addChildren([
   getDegreesRoute,
   aboutRoute,
   authenticatedRoute.addChildren([
-    indexRoute,
+    homeRoute,
     editUserProfileRoute,
     editTimetableRoute,
     finalizeTimetableRoute,
