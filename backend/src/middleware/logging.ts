e6f472d06e330d818ae8bdcdbcddcdb8072e5e14
skoriop <karthikp.ivy@gmail.com>
2024-06-26 12:51:23 +0530

    feat: add response logging

--- backend/src/middleware/logging.ts
+++ backend/src/middleware/logging.ts
@@ -1,12 +1,42 @@
 import { NextFunction, Request, Response } from "express";
+import { default as onFinished } from "on-finished";
+import { Logger } from "pino";
 
-export const logRequest = async (
-  req: Request,
-  res: Response,
+interface ExtendedResponse extends Response {
+  body?: any;
+}
+
+// funny how request needs this but not response
+interface ExtendedRequest extends Request {
+  log: Logger;
+}
+
+export const logger = async (
+  req: ExtendedRequest,
+  res: ExtendedResponse,
   next: NextFunction,
 ) => {
-  const { log, ...rest } = req;
-  log.debug(rest);
-  log.info(`${rest.method} ${req.url}`);
+  const { log: reqLogger, ...rest } = req;
+  reqLogger.debug(rest);
+  reqLogger.info(`${rest.method} ${req.url}`);
+
+  const originalSend = res.json;
+  res.json = (body) => {
+    res.body = body;
+    res.json = originalSend;
+    return res.json(body);
+  };
+
+  onFinished(res, (err, res) => {
+    const { log: resLogger, ...rest } = res;
+    resLogger.debug(rest);
+    if (err) {
+      resLogger.error(`${rest.statusCode} ${err}`);
+    } else {
+      resLogger.info(
+        `${rest.statusCode} ${rest.body?.message ?? res.statusMessage}`,
+      );
+    }
+  });
   next();
 };
