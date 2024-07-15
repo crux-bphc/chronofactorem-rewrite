0d1b1953520564e771f9a77c065844d29ab0b1d9
Samir Khairati <samirk10101@gmail.com>
2024-07-15 19:45:04 +0530

    fix: added a maximum size to the announcements dialog

--- frontend/src/components/announcements.tsx
+++ frontend/src/components/announcements.tsx
@@ -26,35 +26,36 @@ function Announcements() {
 
   return (
     <Dialog open={open} onOpenChange={() => setOpen(!open)}>
-      <DialogContent>
+      <DialogContent className="max-h-[400px] overflow-y-scroll">
         <DialogHeader>
           <DialogTitle className="text-xl">Announcements</DialogTitle>
-          <DialogDescription>
-            <div className="flex mx-3 mt-5 gap-3 flex-col-reverse divide-y divide-y-reverse">
-              {
-                announcements?.length ?
-                  announcements.
-                    sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt))
-                    .map(
-                      (announcement) => (
-                        <div className="flex gap-1 flex-col">
-                          <h1 className="font-bold text-base">
-                            {announcement.title}
-                          </h1>
-                          <p className="opacity-70 text-xs">
-                            {new Date(announcement.createdAt).toLocaleString().slice(0, -3)}
-                          </p>
-                          <p className="opacity-90 mb-3">
-                            {announcement.message}
-                          </p>
-                        </div>
-                      ),
-                    )
-                  : <p>No announcements</p>
-              }
-            </div>
-          </DialogDescription>
         </DialogHeader>
+        <DialogDescription>
+          <div className=" flex flex-col-reverse mx-3 mt-2 gap-3 divide-y divide-y-reverse">
+            {
+              announcements?.length ?
+                announcements.
+                  sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt))
+                  .map(
+                    (announcement) => (
+                      <div className="flex gap-1 flex-col">
+                        <h1 className="font-bold text-base">
+                          {announcement.title}
+                        </h1>
+                        <p className="opacity-70 text-xs">
+                          {new Date(announcement.createdAt).toLocaleString().slice(0, -3)}
+                        </p>
+                        <p className="opacity-90 mb-3">
+                          {announcement.message}
+                        </p>
+                      </div>
+                    ),
+                  )
+                : <p>No announcements</p>
+            }
+          </div>
+        </DialogDescription>
+
       </DialogContent>
     </Dialog>
   );
