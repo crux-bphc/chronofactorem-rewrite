86fc36a6fb6037edd5163c90080f470ef464a8df
Samir Khairati <samirk10101@gmail.com>
2024-07-15 23:02:09 +0530

    feat: Change announcements modal to a button instead of an automatically opening dialog

--- frontend/src/components/announcements.tsx
+++ frontend/src/components/announcements.tsx
@@ -8,9 +8,12 @@ import {
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
+import { DialogTrigger } from "@radix-ui/react-dialog";
+import { Megaphone } from "lucide-react";
 import { useState } from "react";
 import type { z } from "zod";
 import type { announcementType } from "../../../lib/src";
+import { Button } from "./ui/button";
 
 const fetchAnnouncements = async (): Promise<
   z.infer<typeof announcementType>[]
@@ -28,10 +31,13 @@ function Announcements() {
     queryFn: fetchAnnouncements,
   });
 
-  const [open, setOpen] = useState(true);
-
   return (
-    <Dialog open={open} onOpenChange={() => setOpen(!open)}>
+    <Dialog>
+      <DialogTrigger>
+        <Button className="mr-9" variant="outline" size="icon">
+          <Megaphone className="h-5 w-5" />
+        </Button>
+      </DialogTrigger>
       <DialogContent className="max-h-[400px] overflow-y-scroll">
         <DialogHeader>
           <DialogTitle className="text-xl">Announcements</DialogTitle>
@@ -40,14 +46,18 @@ function Announcements() {
           <div className=" flex flex-col-reverse mx-3 mt-2 gap-3 divide-y divide-y-reverse">
             {announcements?.length ? (
               announcements
-                .sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt))
+                .sort(
+                  (a, b) =>
+                    new Date(a.createdAt as string).getTime() -
+                    new Date(b.createdAt as string).getTime(),
+                )
                 .map((announcement) => (
                   <div className="flex gap-1 flex-col">
                     <h1 className="font-bold text-base">
                       {announcement.title}
                     </h1>
                     <p className="opacity-70 text-xs">
-                      {new Date(announcement.createdAt)
+                      {new Date(announcement.createdAt as string)
                         .toLocaleString()
                         .slice(0, -3)}
                     </p>
--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -146,104 +146,102 @@ export function NavBar() {
   const renderNavbarBasedOnQueryFetch = (
     userQueryResultData: typeof userQueryResult.data,
   ) => (
-    <>
-      <Announcements />
-      <div className="flex flex-row w-full justify-between shadow-lg">
-        <div className="flex items-center">
-          <Link to={userQueryResultData ? "/" : undefined}>
-            <div className="hidden md:flex">
-              <h1 className="scroll-m-20 cursor-pointer text-2xl font-extrabold tracking-tight lg:text-3xl m-4 text-foreground">
-                ChronoFactorem
-              </h1>
-            </div>
-            <div className="flex md:hidden">
-              <h1 className="scroll-m-20 cursor-pointer text-2xl font-extrabold tracking-tight lg:text-3xl mx-2 my-4 text-foreground">
-                Chrono
-              </h1>
-            </div>
-          </Link>
-          {!isEditPage && (
-            <Button
-              className="text-green-200 w-fit text-xl p-4 ml-4 bg-green-900 hover:bg-green-800"
-              onClick={
-                userQueryResultData ? () => createMutation.mutate() : undefined
-              }
-            >
-              <div className="hidden md:flex">Create a timetable</div>
-              <div className="flex md:hidden">
-                <Plus className="h-6 w-6" />
-              </div>
-            </Button>
-          )}
-          <Link
-            to={userQueryResultData ? "/about" : undefined}
-            className="text-primary py-2 px-2 ml-2 text-lg rounded-full hover:bg-muted transition h-fit duration-200 ease-in-out"
-          >
-            <div className="hidden md:flex">About</div>
-            <div className="flex md:hidden">
-              <Info className="h-6 w-6" />
-            </div>
-          </Link>
-          {!isCMSPage && (
-            <Link
-              to={userQueryResultData ? "/CMSExport" : undefined}
-              className="text-primary py-2 px-2 ml-2 text-lg rounded-full hover:bg-muted transition h-fit whitespace-nowrap duration-200 ease-in-out"
-            >
-              <div className="hidden md:flex">CMS Auto-Enroll</div>
-              <div className="flex md:hidden">
-                <BookUp className="h-6 w-6" />
-              </div>
-            </Link>
-          )}
-        </div>
-        <div className="flex flex-row">
-          <div className="pt-3">
-            <ModeToggle />
+    <div className="flex flex-row w-full justify-between shadow-lg">
+      <div className="flex items-center">
+        <Link to={userQueryResultData ? "/" : undefined}>
+          <div className="hidden md:flex">
+            <h1 className="scroll-m-20 cursor-pointer text-2xl font-extrabold tracking-tight lg:text-3xl m-4 text-foreground">
+              ChronoFactorem
+            </h1>
           </div>
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <div className="rounded-full text-foreground bg-accent p-1 px-3 text-xl h-fit lg:mx-8 mx-2 mt-4">
-                <span>
-                  {userQueryResultData ? userQueryResultData.name[0] : " "}
-                </span>
-              </div>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent className="lg:w-56 w-fit">
-              <DropdownMenuLabel>My Account</DropdownMenuLabel>
-              <DropdownMenuItem asChild>
-                <Link to={userQueryResultData ? "/editProfile" : undefined}>
-                  <Pencil className="mr-2 h-4 w-4" />
-                  <span>Edit Profile</span>
-                </Link>
-              </DropdownMenuItem>
-              <DropdownMenuItem
-                asChild
-                className="focus:bg-destructive/90 focus:text-destructive-foreground cursor-pointer"
-                onClick={() => {
-                  const frontendIsHTTPS =
-                    import.meta.env.VITE_FRONTEND_URL.includes("https://");
-                  removeCookie("session", {
-                    path: "/",
-                    domain: import.meta.env.VITE_FRONTEND_URL.replace(
-                      frontendIsHTTPS ? "https://" : "http://",
-                      "",
-                    ),
-                  });
-                  router.navigate({
-                    to: "/login",
-                  });
-                }}
-              >
-                <div>
-                  <LogOut className="mr-2 h-4 w-4" />
-                  <span>Log out</span>
-                </div>
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
-        </div>
+          <div className="flex md:hidden">
+            <h1 className="scroll-m-20 cursor-pointer text-2xl font-extrabold tracking-tight lg:text-3xl mx-2 my-4 text-foreground">
+              Chrono
+            </h1>
+          </div>
+        </Link>
+        {!isEditPage && (
+          <Button
+            className="text-green-200 w-fit text-xl p-4 ml-4 bg-green-900 hover:bg-green-800"
+            onClick={
+              userQueryResultData ? () => createMutation.mutate() : undefined
+            }
+          >
+            <div className="hidden md:flex">Create a timetable</div>
+            <div className="flex md:hidden">
+              <Plus className="h-6 w-6" />
+            </div>
+          </Button>
+        )}
+        <Link
+          to={userQueryResultData ? "/about" : undefined}
+          className="text-primary py-2 px-2 ml-2 text-lg rounded-full hover:bg-muted transition h-fit duration-200 ease-in-out"
+        >
+          <div className="hidden md:flex">About</div>
+          <div className="flex md:hidden">
+            <Info className="h-6 w-6" />
+          </div>
+        </Link>
+        {!isCMSPage && (
+          <Link
+            to={userQueryResultData ? "/CMSExport" : undefined}
+            className="text-primary py-2 px-2 ml-2 text-lg rounded-full hover:bg-muted transition h-fit whitespace-nowrap duration-200 ease-in-out"
+          >
+            <div className="hidden md:flex">CMS Auto-Enroll</div>
+            <div className="flex md:hidden">
+              <BookUp className="h-6 w-6" />
+            </div>
+          </Link>
+        )}
       </div>
-    </>
+      <div className="flex flex-row">
+        <div className="pt-3">
+          <Announcements />
+          <ModeToggle />
+        </div>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <div className="rounded-full text-foreground bg-accent p-1 px-3 text-xl h-fit lg:mx-8 mx-2 mt-4">
+              <span>
+                {userQueryResultData ? userQueryResultData.name[0] : " "}
+              </span>
+            </div>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="lg:w-56 w-fit">
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuItem asChild>
+              <Link to={userQueryResultData ? "/editProfile" : undefined}>
+                <Pencil className="mr-2 h-4 w-4" />
+                <span>Edit Profile</span>
+              </Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem
+              asChild
+              className="focus:bg-destructive/90 focus:text-destructive-foreground cursor-pointer"
+              onClick={() => {
+                const frontendIsHTTPS =
+                  import.meta.env.VITE_FRONTEND_URL.includes("https://");
+                removeCookie("session", {
+                  path: "/",
+                  domain: import.meta.env.VITE_FRONTEND_URL.replace(
+                    frontendIsHTTPS ? "https://" : "http://",
+                    "",
+                  ),
+                });
+                router.navigate({
+                  to: "/login",
+                });
+              }}
+            >
+              <div>
+                <LogOut className="mr-2 h-4 w-4" />
+                <span>Log out</span>
+              </div>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </div>
   );
   if (userQueryResult.isFetching) {
     return renderNavbarBasedOnQueryFetch(undefined);
