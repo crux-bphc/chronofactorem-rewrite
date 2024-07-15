08ce41c81af0d17f5307c6e76a18a835c29b1a4a
Samir Khairati <samirk10101@gmail.com>
2024-07-15 19:09:53 +0530

    fix: Show 'no announcements' when there are no announcements instead of not rendering the dialog at all

--- frontend/src/components/announcements.tsx
+++ frontend/src/components/announcements.tsx
@@ -10,45 +10,46 @@ import axios from "axios";
 
 import { useState } from "react";
 
+const fetchAnnouncements = async () => {
+  const response = await axios.get("api/user/announcements");
+  console.log(response.data);
+  return response.data;
+}
+
 function Announcements() {
   const { data: announcements } = useQuery({
     queryKey: ["announcements"],
-    queryFn: async () => {
-      const response = await axios.get("api/user/announcements");
-      return response.data;
-    },
+    queryFn: fetchAnnouncements,
   });
 
   const [open, setOpen] = useState(true);
 
   return (
-    <>
-      {announcements?.length && (
-        <Dialog open={open} onOpenChange={() => setOpen(!open)}>
-          <DialogContent>
-            <DialogHeader>
-              <DialogTitle>Announcements</DialogTitle>
-              <DialogDescription>
-                {
-                  <div className="flex mx-3 mt-5 gap-3 flex-col-reverse divide-y divide-y-reverse">
-                    {announcements.map(
-                      (announcement: { title: string; message: string }) => (
-                        <div className="flex gap-1 flex-col">
-                          <h1 className="font-bold">{announcement.title}</h1>
-                          <p className="opacity-90 mb-3">
-                            {announcement.message}
-                          </p>
-                        </div>
-                      ),
-                    )}
-                  </div>
-                }
-              </DialogDescription>
-            </DialogHeader>
-          </DialogContent>
-        </Dialog>
-      )}
-    </>
+    <Dialog open={open} onOpenChange={() => setOpen(!open)}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Announcements</DialogTitle>
+          <DialogDescription>
+            <div className="flex mx-3 mt-5 gap-3 flex-col-reverse divide-y divide-y-reverse">
+              {
+                announcements?.length ?
+                  announcements.map(
+                    (announcement: { title: string; message: string }) => (
+                      <div className="flex gap-1 flex-col">
+                        <h1 className="font-bold">{announcement.title}</h1>
+                        <p className="opacity-90 mb-3">
+                          {announcement.message}
+                        </p>
+                      </div>
+                    ),
+                  )
+                  : <p>No announcements</p>
+              }
+            </div>
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
   );
 }
 
