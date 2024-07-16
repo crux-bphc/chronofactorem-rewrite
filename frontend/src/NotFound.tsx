edc87a08352088cdac9d6f6d356b707d40b0bb75
Samir Khairati <samirk10101@gmail.com>
2024-07-17 00:19:56 +0530

    fix: color changes in NotFound.tsx

--- frontend/src/NotFound.tsx
+++ frontend/src/NotFound.tsx
@@ -6,7 +6,7 @@ const notFoundRoute = new Route({
   path: "404",
   component: () => {
     return <>
-      <div className="h-screen w-full flex flex-col justify-center items-center bg-[#1A2238]">
+      <div className="h-screen w-full flex flex-col justify-center items-center">
         <h1 className="text-9xl font-extrabold text-white tracking-widest">404</h1>
         <div className="bg-[#FF6A3D] px-2 text-sm rounded rotate-12 absolute">
           Page Not Found
