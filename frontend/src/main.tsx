4792985e23c9fc46d94df03f082b7f05d0f83c5e
skoriop <karthikp.ivy@gmail.com>
2024-07-17 13:45:08 +0530

    feat: use NotFound component instead of route

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -19,7 +19,7 @@ import finalizeTimetableRoute from "./FinalizeTimetable";
 import getDegreesRoute from "./GetDegrees";
 import homeRoute from "./Home";
 import loginRoute from "./Login";
-import notFoundRoute from "./NotFound";
+import NotFound from "./components/NotFound";
 import RootComponent from "./RootComponent";
 import viewTimetableRoute from "./ViewTimetable";
 import "./index.css";
@@ -63,7 +63,7 @@ export const router = new Router({
   context: {
     queryClient,
   },
-  notFoundRoute: notFoundRoute,
+  defaultNotFoundComponent: NotFound
 });
 
 // Register things for typesafety
