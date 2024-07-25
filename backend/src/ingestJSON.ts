f08baeab3c41b8f70392a83a1ef7e99929bc7243
skoriop <karthikp.ivy@gmail.com>
2024-07-25 21:37:36 +0530

    feat: update timetables in search service

--- backend/src/ingestJSON.ts
+++ backend/src/ingestJSON.ts
@@ -2,6 +2,7 @@ import { QueryRunner } from "typeorm";
 import { sectionTypeEnum } from "../../lib/src/index.js";
 import { env } from "./config/server.js";
 import { Course, Section, Timetable } from "./entity/entities.js";
+import sqids from "./utils/sqids.js";
 
 interface ExamJSON {
   midsem: string | null;
@@ -395,6 +396,70 @@ export const ingestJSON = async (
       }
     }
     console.log("added updated courses to search service!");
+
+    console.log("updating timetables in search service...");
+    const [timetableIds, timetableCount] = await queryRunner.manager
+      .createQueryBuilder()
+      .select("timetable.id")
+      .from(Timetable, "timetable")
+      .where("timetable.archived = :archived", { archived: true })
+      .getManyAndCount();
+    console.log(`${timetableCount} timetables are to be updated`);
+    for (const { id } of timetableIds) {
+      const encodedId = sqids.encode([id]);
+      try {
+        const searchServiceURL = `${env.SEARCH_SERVICE_URL}/timetable/remove`;
+        const res = await fetch(searchServiceURL, {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ id: encodedId }),
+        });
+        if (!res.ok) {
+          const resJson = await res.json();
+          console.log(
+            `error while removing timetable ${id} from search service: ${resJson.error}`,
+          );
+        }
+      } catch (err: any) {
+        console.log(
+          `error while removing timetable ${id} from search service: ${err.message}`,
+        );
+      }
+      const timetable = await queryRunner.manager
+        .createQueryBuilder()
+        .select("timetable")
+        .from(Timetable, "timetable")
+        .leftJoinAndSelect("timetable.sections", "section")
+        .where("timetable.id = :id", { id })
+        .getOne();
+      const timetableWithSqid = {
+        ...timetable,
+        id: encodedId,
+      };
+      try {
+        const searchServiceURL = `${env.SEARCH_SERVICE_URL}/timetable/add`;
+        const res = await fetch(searchServiceURL, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(timetableWithSqid),
+        });
+        if (!res.ok) {
+          const resJson = await res.json();
+          console.log(
+            `error while adding timetable ${id} to search service: ${resJson.error}`,
+          );
+        }
+      } catch (err: any) {
+        console.log(
+          `error while adding timetable ${id} to search service: ${err.message}`,
+        );
+      }
+    }
+    console.log("updated timetables in search service!");
 
     await queryRunner.commitTransaction();
     // show summary of the transaction
