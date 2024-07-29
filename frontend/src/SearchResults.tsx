96da147c0550b09e5feed7ec79ee4e928715551b
Samir Khairati <samirk10101@gmail.com>
2024-07-30 00:54:08 +0530

    feat: add search component, badges, redirect to view timetable

--- frontend/src/SearchResults.tsx
+++ frontend/src/SearchResults.tsx
@@ -1,12 +1,23 @@
 import { queryOptions, useQuery } from "@tanstack/react-query";
 import { ErrorComponent, Route } from "@tanstack/react-router";
 import axios, { AxiosError } from "axios";
-import { z } from "zod";
-import { timetableWithSectionsType } from "../../lib/src";
+import type { z } from "zod";
+import type { timetableWithSectionsType } from "../../lib/src";
 import authenticatedRoute from "./AuthenticatedRoute";
 import { ToastAction } from "./components/ui/toast";
 import { useToast } from "./components/ui/use-toast";
 import { router } from "./main";
+import { useEffect } from "react";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Badge } from "@/components/ui/badge"
 
 const fetchSearchDetails = async (
   query: string,
@@ -30,7 +41,7 @@ const searchQueryOptions = (query: string) =>
 
 const searchRoute = new Route({
   getParentRoute: () => authenticatedRoute,
-  path: "search/$query",
+  path: "/search/$query",
   component: SearchResults,
   loader: ({ context: { queryClient }, params }) =>
     queryClient
@@ -117,11 +128,41 @@ function SearchResults() {
   const { query } = searchRoute.useParams();
   // @ts-ignore Suppress unused variable warning, needs to be removed when the page is finished
   const searchQueryResult = useQuery(searchQueryOptions(query));
+
+  useEffect(() => {
+    console.log(searchQueryResult.data);
+  }, [searchQueryResult]);
+
   return (
     <main className="text-foreground py-6 md:py-12 px-10 md:px-16">
       <h1 className="text-xl font-bold text-center sm:text-left md:text-4xl">
         Search Results
       </h1>
+      <div className="my-10 flex flex-wrap gap-5">
+        {
+          searchQueryResult.data?.map((timetable) => {
+            return (
+              <Card key={timetable.id} className="w-md cursor-pointer" onClick={() => router.navigate({ to: `/view/${timetable.id}` })}>
+                <CardHeader>
+                  <CardTitle>{timetable.name}</CardTitle>
+                  <CardDescription>By: {timetable.authorId}</CardDescription>
+                </CardHeader>
+                <CardContent>
+                  <div className="flex gap-2">
+                    <Badge>Year {timetable.year}</Badge>
+                    <Badge>Sem {timetable.semester}</Badge>
+                    <Badge>{timetable.acadYear}</Badge>
+                    <Badge>{timetable.degrees}</Badge>
+                    {timetable.archived ? <Badge variant='destructive'>Archived</Badge> : null}
+                  </div>
+                </CardContent>
+              </Card>
+            );
+          })
+        }
+      </div>
+
+
     </main>
   );
 }
