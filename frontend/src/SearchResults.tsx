d0c2e2ec684b843dcec915914d75162a082b7ae7
skoriop <karthikp.ivy@gmail.com>
2024-07-30 10:50:19 +0530

    feat: add page number

--- frontend/src/SearchResults.tsx
+++ frontend/src/SearchResults.tsx
@@ -146,40 +146,45 @@ function SearchResults() {
 
   return (
     <main className="text-foreground py-6 md:py-12 px-10 md:px-16">
-      <div className="w-full flex gap-2 justify-between">
+      <div className="w-full flex gap-2 justify-between items-center">
         <h1 className="text-xl font-bold text-center sm:text-left md:text-4xl">
           Search Results
         </h1>
-        <Pagination className="w-fit mx-0 text-2xl">
-          <PaginationContent>
-            <PaginationItem>
-              <PaginationPrevious
-                onClick={() =>
-                  setDeps((deps) => ({
-                    ...deps,
-                    page: Math.max(
-                      0,
-                      ((deps.page as number | undefined) ?? 0) - 1,
-                    ),
-                  }))
-                }
-              />
-            </PaginationItem>
-            <PaginationItem>
-              <PaginationNext
-                onClick={() =>
-                  setDeps((deps) => ({
-                    ...deps,
-                    page: Math.min(
-                      50,
-                      ((deps.page as number | undefined) ?? 0) + 1,
-                    ),
-                  }))
-                }
-              />
-            </PaginationItem>
-          </PaginationContent>
-        </Pagination>
+        <div className="flex flex-col items-center">
+          <h2 className="text-muted-foreground font-bold">
+            Page {(deps.page as number ?? 0) + 1}
+          </h2>
+          <Pagination className="w-fit mx-0 text-2xl text-foreground">
+            <PaginationContent>
+              <PaginationItem>
+                <PaginationPrevious
+                  onClick={() =>
+                    setDeps((deps) => ({
+                      ...deps,
+                      page: Math.max(
+                        0,
+                        ((deps.page as number | undefined) ?? 0) - 1,
+                      ),
+                    }))
+                  }
+                />
+              </PaginationItem>
+              <PaginationItem>
+                <PaginationNext
+                  onClick={() =>
+                    setDeps((deps) => ({
+                      ...deps,
+                      page: Math.min(
+                        50,
+                        ((deps.page as number | undefined) ?? 0) + 1,
+                      ),
+                    }))
+                  }
+                />
+              </PaginationItem>
+            </PaginationContent>
+          </Pagination>
+        </div>
       </div>
       <div className="my-10 grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-5">
         {searchQueryResult.data?.map((timetable) => {
@@ -220,7 +225,7 @@ function SearchResults() {
         ) : null}
         {searchQueryResult.data?.length === 0 ? (
           <div className="w-md h-96 bg-background">
-            <p className="text-center text-lg font-bold text-muted-foreground">
+            <p className="text-lg font-bold text-muted-foreground">
               No results found
             </p>
           </div>
