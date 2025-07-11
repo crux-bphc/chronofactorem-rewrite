bcdcc16cc28b01e9f7a28b31a994ec301f6c3de1
soumitradev <soumitra.shewale.dev@gmail.com>
2025-07-11 14:52:40 +0000

    feat: restore home link on NotFound component

--- frontend/src/components/NotFound.tsx
+++ frontend/src/components/NotFound.tsx
@@ -1,3 +1,5 @@
+import { Link } from "@tanstack/react-router";
+
 const NotFound = () => (
   <div className="h-[calc(100vh-68px)] w-full flex flex-col justify-center items-center">
     <h1 className="text-9xl font-extrabold text-white tracking-widest">404</h1>
@@ -8,12 +10,12 @@ const NotFound = () => (
       <div className="relative inline-block text-sm font-medium text-[#FF6A3D] group active:text-orange-500 focus:outline-none focus:ring">
         <span className="absolute inset-0 transition-transform translate-x-0.5 translate-y-0.5 bg-[#FF6A3D] group-hover:translate-y-0 group-hover:translate-x-0" />
 
-        {/* <span
-          onClick={() => router.navigate({ to: "/" })}
+        <Link
           className="cursor-pointer relative block px-8 py-3 bg-[#1A2238] border border-current"
+          to={"/"}
         >
           GO HOME
-        </span> */}
+        </Link>
       </div>
     </div>
   </div>
