fe536d6fc884f097ed68355158b6371ef57e5a70
skoriop <karthikp.ivy@gmail.com>
2025-07-13 20:28:39 +0530

    feat: improve report issue styling

--- frontend/src/components/ReportIssue.tsx
+++ frontend/src/components/ReportIssue.tsx
@@ -1,9 +1,16 @@
+import { ExternalLink } from "lucide-react";
+
 const ReportIssue = ({ error }: { error: string }) => (
   <span>
-    Unexpected error: {error} Please report this{" "}
-    <a href="https://github.com/crux-bphc/chronofactorem-rewrite/issues">
-      here
-    </a>
+    Unexpected error: {error}
+    <br />
+    Please report this{" "}
+    <span className="inline-flex items-baseline gap-1 text-blue-600 dark:text-blue-500 hover:underline">
+      <a href="https://github.com/crux-bphc/chronofactorem-rewrite/issues">
+        here
+      </a>
+      <ExternalLink size={16} />
+    </span>
   </span>
 );
 
