c1c88775bf140dbd001aac341ce2e8f3fe9fd999
soumitradev <soumitra.shewale.dev@gmail.com>
2026-07-23 08:31:18 +0000

    chore: remove dead comments and stray params on navigate to home

--- frontend/src/pages/FinalizeTimetable.tsx
+++ frontend/src/pages/FinalizeTimetable.tsx
@@ -156,7 +156,6 @@ function FinalizeTimetable() {
                 onSuccess: () => {
                   router.navigate({
                     to: "/",
-                    params: { timetableId: timetableId },
                   });
                 },
               },
